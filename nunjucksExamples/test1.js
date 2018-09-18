697cd1a8f378c9e5ac4fb5593d8cd158a117de85
krunaljoshi <kjoshi6@horizon.csueastbay.edu>
2018-09-17 18:52:58 -0700

    Branch Created

--- nunjucksExamples/test1.js
+++
@@ -1,8 +0,0 @@
-/* A file to see if *nunjucks* is installed and
-    whether we can run a simple example.
-*/
-
-nunjucks = require('nunjucks');
-nunjucks.configure({ autoescape: true });
-let helloStr = nunjucks.renderString('Hello {{ username }}', { username: 'Dr. B.' });
-console.log(helloStr);
